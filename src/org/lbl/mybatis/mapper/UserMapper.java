7886c7647628e0006c8d6be17b2bb8bb9ae777c1
libaolei007 <821342102@qq.com>
2019-05-29 21:20:57 +0800

    mybatis语法

--- src/org/lbl/mybatis/mapper/UserMapper.java
+++ src/org/lbl/mybatis/mapper/UserMapper.java
@@ -13,13 +13,36 @@ public interface UserMapper {
      */
     public int insertUser(User user) throws Exception;
     /**
-     * 修改用戶
-     * @param user
-     * @param id
+     * 批量新增用戶
+     * @param users
      * @return
      * @throws Exception
      */
-    public int updateUser (User user,int id) throws Exception;
+    public int insertUsers(List<User> users) throws Exception;
+    /**
+     * 修改用戶
+     * @param user
+     * @return
+     * @throws Exception
+     */
+    public int updateUser (User user) throws Exception;
+
+    /**
+     * 修改用戶
+     * @param user
+     * @return
+     * @throws Exception
+     */
+    public int updateUserSet (User user) throws Exception;
+
+    /**
+     * 批量修改用戶
+     * @param users
+     * @return
+     * @throws Exception
+     */
+    public int updateUsers (List<User> users) throws Exception;
+
     /**
      * 刪除用戶
      * @param id
@@ -27,6 +50,15 @@ public interface UserMapper {
      * @throws Exception
      */
     public int deleteUser(int id) throws Exception;
+
+    /**
+     * 批量刪除用戶
+     * @param ids
+     * @return
+     * @throws Exception
+     */
+
+    public int deleteBatch(List<Integer> ids) throws Exception;
     /**
      * 根据id查询用户信息
      * @param id
@@ -40,5 +72,12 @@ public interface UserMapper {
      * @throws Exception
      */
     public List<User> selectAllUser() throws Exception;
+
+    /**
+     * 根据Id查询用户信息
+     * @return
+     * @throws Exception
+     */
+    public List<User> getUserByIds(List<String> ids) throws Exception;
 
 }
--- src/org/lbl/mybatis/service/UserServiceTest.java
+++ src/org/lbl/mybatis/service/UserServiceTest.java
@@ -6,6 +6,7 @@ import org.lbl.mybatis.beans.User;
 import org.lbl.mybatis.mapper.UserMapper;
 import org.lbl.mybatis.utils.DBUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserServiceTest {
@@ -26,18 +27,24 @@ public class UserServiceTest {
     }
 
     @Test
-    //删除操作
-    public void deleteUser() throws Exception {
-        SqlSession session=DBUtils.getSession();
-        UserMapper mapper=session.getMapper(UserMapper.class);
+    //批量添加操作
+    public void insertUsers() throws Exception {
+        SqlSession session = DBUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User("香蕉001", "8888", 8888.8);
+        User user1 = new User("香蕉002", "8888", 8888.8);
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user1);
         try {
-            mapper.deleteUser(1);
+            mapper.insertUsers(users);
             session.commit();
         } catch (Exception e) {
             e.printStackTrace();
             session.rollback();
         }
     }
+
 
     @Test
     //选择操作
@@ -48,6 +55,61 @@ public class UserServiceTest {
             //这里用到User类的无参构造方法创建对象，User user=new User（）；
             User user =mapper.selectUserById(2);
             System.out.println(user.toString());
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+    @Test
+    public void updateUser() throws Exception{
+        SqlSession session = DBUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User("橘子","111",222.2);
+        user.setId(1);
+        try {
+           int result =  mapper.updateUser(user);
+            session.commit();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+
+    @Test
+    public void updateUserSet() throws Exception{
+        SqlSession session = DBUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User("橘子","111",222.2);
+        user.setId(5);
+        try {
+            int result =  mapper.updateUserSet(user);
+            session.commit();
+            System.out.println(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+
+    @Test
+    public void updateUsers(){
+        SqlSession session = DBUtils.getSession();
+        UserMapper mapper = session.getMapper(UserMapper.class);
+        User user = new User("橘子111111","111",222.2);
+        user.setId(1);
+        User user1 = new User("橘子222222","222",222.2);
+        user1.setId(2);
+        List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user1);
+        try {
+            int result =  mapper.updateUsers(users);
+            System.out.println(result);
             session.commit();
         } catch (Exception e) {
             e.printStackTrace();
@@ -69,4 +131,54 @@ public class UserServiceTest {
         }
     }
 
+
+    @Test
+    public void getUserByIds() throws Exception {
+        SqlSession session= DBUtils.getSession();
+        UserMapper mapper=session.getMapper(UserMapper.class);
+        List<String> ids = new ArrayList<>();
+        ids.add("1");
+        ids.add("2");
+        try {
+            List<User> user=mapper.getUserByIds(ids);
+            System.out.println(user.toString());
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+
+    @Test
+    //删除操作
+    public void deleteUser() throws Exception {
+        SqlSession session=DBUtils.getSession();
+        UserMapper mapper=session.getMapper(UserMapper.class);
+        try {
+            mapper.deleteUser(1);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
+
+
+    @Test
+    //删除操作
+    public void deleteBatch() throws Exception {
+        SqlSession session=DBUtils.getSession();
+        UserMapper mapper=session.getMapper(UserMapper.class);
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(2);
+        try {
+            mapper.deleteBatch(list);
+            session.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            session.rollback();
+        }
+    }
 }
